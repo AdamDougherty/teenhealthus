a665394cbd8b95a46073a5941eb8a64a3595e008
Adam Dougherty <user@example.com>
2026-03-26 15:42:30 -0700

    Reduce image crop to show more of the top

--- app/volunteer/page.tsx
+++ app/volunteer/page.tsx
@@ -268,7 +268,7 @@ export default function VolunteerPage() {
                   src="/images/shared/Newer Pictures/long-tent.jpg"
                   alt="Teen Health volunteers at an event"
                   fill
-                  className="object-cover" style={{ objectPosition: 'center 70%' }}
+                  className="object-cover" style={{ objectPosition: 'center 45%' }}
                   sizes="(max-width: 768px) 100vw, 50vw"
                 />
               </div>
